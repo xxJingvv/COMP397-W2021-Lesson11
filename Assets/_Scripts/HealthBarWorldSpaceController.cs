b6890e1eced4e00d185ff310d814673db09b85b6
Jing <sanmerge@gmail.com>
2021-04-10 21:59:11 -0400

    updated

---
+++ Assets/_Scripts/HealthBarWorldSpaceController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarWorldSpaceController : MonoBehaviour
+{
+    public Transform playerCameraTransform;
+    public GameObject playerCamera;
+
+    void Start()
+    {
+        playerCamera = GameObject.Find("PlayerCamera");
+
+        if (playerCamera)
+        {
+            playerCameraTransform = playerCamera.transform;
+        }
+    }
+
+
+    void LateUpdate()
+    {
+        // billboard the healthBar
+        if(playerCameraTransform)
+        {
+            transform.LookAt(transform.position + playerCameraTransform.forward);
+        }
+    }
+}
